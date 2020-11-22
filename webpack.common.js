02eaccdbacd95a3419c1f44eb8a33eaf89ddbbc7
Imad Elmahrad <70642895+ImadEM21@users.noreply.github.com>
2020-11-22 14:24:39 +0100

    Update webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const htmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const { loadavg } = require('os');
 
 module.exports = {
     entry: {
